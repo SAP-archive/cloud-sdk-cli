65aee66010acb9e4820135e8c73d97c767bea506
Frank Essenberger <frank.essenberger@sap.com>
2019-10-21 10:23:06 +0200

    remeve unnecessary jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/lib', '<rootDir>/bin', '<rootDir>/tmp', '<rootDir>/test/nest', '<rootDir>/test/express'],
-  transform: {"^.+\\.tsx?$": "ts-jest"},
-  testMatch: ["**/*.spec.ts"],
-  roots: ["test"],
-  reporters: ['default']
+  roots: ["test"]
 };
