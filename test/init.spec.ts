7dda2118b6b7fabaffef3f70a05640f876d9a03f
Florian Richter <florian.richter@sap.com>
2019-12-06 17:00:12 +0100

    Ask for usage analytics

--- test/init.spec.ts
+++ test/init.spec.ts
@@ -27,8 +27,19 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import Init from '../src/commands/init';
 
+const pathPrefix = path.resolve(__dirname, __filename.replace(/\./g, '-')).replace('-ts', '');
+
+function getCleanProjectDir(name: string) {
+  const projectDir = path.resolve(pathPrefix, name);
+  if (fs.existsSync(projectDir)) {
+    fs.removeSync(projectDir);
+  }
+  return projectDir;
+}
+
 describe('Init', () => {
-  const pathPrefix = path.resolve(__dirname, __filename.replace(/\./g, '-')).replace('-ts', '');
+  const expressAppDir = 'test/express/';
+  const nestAppDir = 'test/nest/';
 
   beforeAll(() => {
     if (!fs.existsSync(pathPrefix)) {
@@ -41,87 +52,49 @@ describe('Init', () => {
   });
 
   it('should create a new project with the necessary files', async () => {
-    const projectDir = path.resolve(pathPrefix, 'full-init');
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
+    const projectDir = getCleanProjectDir('full-init');
 
-    const argv = ['--projectName=testingApp', '--buildScaffold', `--projectDir=${projectDir}`];
-    await Init.run(argv);
+    await Init.run(['--projectName=testingApp', '--buildScaffold', `--projectDir=${projectDir}`]);
 
     ['.npmrc', 'credentials.json', 'systems.json', 'manifest.yml']
       .map(file => path.resolve(projectDir, file))
       .forEach(path => {
         expect(fs.existsSync(path)).toBe(true);
       });
-  }, 60000);
-
-  it('should create test cases when building a project with scaffold', async () => {
-    const projectDir = path.resolve(pathPrefix, 'full-init-with-test');
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
-
-    const argv = ['--projectName=testingApp', '--buildScaffold', `--projectDir=${projectDir}`];
-    await Init.run(argv);
 
     const reportsPath = path.resolve(projectDir, 's4hana_pipeline', 'reports');
 
     // execute the ci scripts and check if the reports are written
     await execa('npm', ['run', 'ci-backend-unit-test'], { cwd: projectDir, stdio: 'inherit' });
-
-    const pathBackendUnit = path.resolve(reportsPath, 'backend-unit');
-    const pathCoverageUnit = path.resolve(reportsPath, 'coverage-reports', 'backend-unit');
-    expect(fs.readdirSync(pathBackendUnit).length).toBeGreaterThan(1);
-    expect(fs.readdirSync(pathCoverageUnit).length).toBeGreaterThan(1);
+    expect(fs.readdirSync(path.resolve(reportsPath, 'backend-unit')).length).toBeGreaterThan(1);
+    expect(fs.readdirSync(path.resolve(reportsPath, 'coverage-reports', 'backend-unit')).length).toBeGreaterThan(1);
 
     await execa('npm', ['run', 'ci-integration-test'], { cwd: projectDir, stdio: 'inherit' });
-
-    const pathBackendIntegration = path.resolve(reportsPath, 'backend-integration');
-    const pathCoverageIntegration = path.resolve(reportsPath, 'coverage-reports', 'backend-integration');
-    console.log('Backend Integration', fs.readdirSync(pathBackendIntegration));
-    console.log('Coverage Integration', fs.readdirSync(pathCoverageIntegration));
-    expect(fs.readdirSync(pathBackendIntegration).length).toBeGreaterThan(1);
-    expect(fs.readdirSync(pathCoverageIntegration).length).toBeGreaterThan(1);
-  }, 60000);
+    expect(fs.readdirSync(path.resolve(reportsPath, 'backend-integration')).length).toBeGreaterThan(1);
+    expect(fs.readdirSync(path.resolve(reportsPath, 'coverage-reports', 'backend-integration')).length).toBeGreaterThan(1);
+  }, 120000);
 
   it('should add necessary files to an existing project', async () => {
-    const expressAppDir = 'test/express/';
-    const projectDir = path.resolve(pathPrefix, 'add-to-existing');
+    const projectDir = getCleanProjectDir('add-to-existing');
 
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
     fs.copySync(expressAppDir, projectDir, { recursive: true });
 
-    const argv = ['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`, '--force'];
-    await Init.run(argv);
+    await Init.run(['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`, '--force']);
+
     ['.npmrc', 'credentials.json', 'systems.json', 'manifest.yml']
       .map(file => path.resolve(projectDir, file))
       .forEach(path => {
         expect(fs.existsSync(path)).toBe(true);
       });
-
-    ['jest.config.js', 'jest.integration-test.config.js', 'jets.unit-test.config.js']
-      .map(file => path.resolve(projectDir, file))
-      .forEach(path => {
-        expect(fs.existsSync(path)).toBe(false);
-      });
-
     expect(fs.existsSync(path.resolve(projectDir, 'test'))).toBe(false);
   }, 20000);
 
   it('init should detect and fail if there are conflicts', async () => {
-    const appDir = 'test/nest/';
-    const projectDir = path.resolve(pathPrefix, 'detect-conflicts');
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
-    fs.copySync(appDir, projectDir, { recursive: true });
+    const projectDir = getCleanProjectDir('detect-conflicts');
+    fs.copySync(nestAppDir, projectDir, { recursive: true });
     fs.createFileSync(`${projectDir}/.npmrc`);
 
-    const argv = ['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`];
-    await Init.run(argv);
+    await Init.run(['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`]);
 
     expect(error).toHaveBeenCalledWith(
       'A file with the name ".npmrc" already exists. If you want to overwrite it, rerun the command with `--force`.',
@@ -129,16 +102,11 @@ describe('Init', () => {
     );
   }, 60000);
 
-  it('should add to gitignore if there is one', async () => {
-    const exampleAppDir = 'test/nest/';
-    const projectDir = path.resolve(pathPrefix, 'add-to-gitignore');
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
-    fs.copySync(exampleAppDir, projectDir, { recursive: true });
+  it('should add to .gitignore if there is one', async () => {
+    const projectDir = getCleanProjectDir('add-to-gitignore');
+    fs.copySync(nestAppDir, projectDir, { recursive: true });
 
-    const argv = ['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`];
-    await Init.run(argv);
+    await Init.run(['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`]);
 
     const gitignoreEntries = fs
       .readFileSync(`${projectDir}/.gitignore`, 'utf8')
@@ -152,31 +120,23 @@ describe('Init', () => {
   }, 50000);
 
   it('should show a warning if the project is not using git', async () => {
-    const projectDir = path.resolve(pathPrefix, 'warn-on-no-git');
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
+    const projectDir = getCleanProjectDir('warn-on-no-git');
 
     fs.createFileSync(path.resolve(projectDir, 'package.json'));
     fs.writeFileSync(path.resolve(projectDir, 'package.json'), JSON.stringify({ name: 'project' }), 'utf8');
 
-    const argv = ['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`];
-    await Init.run(argv);
+    await Init.run(['--projectName=testingApp', '--startCommand="npm start"', `--projectDir=${projectDir}`]);
 
     expect(warn).toHaveBeenCalledWith('No .gitignore file found!');
   }, 30000);
 
   it('should add our scripts and dependencies to the package.json', async () => {
-    const projectDir = path.resolve(pathPrefix, 'add-scripts-and-dependencies');
-    if (fs.existsSync(projectDir)) {
-      fs.removeSync(projectDir);
-    }
+    const projectDir = getCleanProjectDir('add-scripts-and-dependencies');
 
     fs.createFileSync(path.resolve(projectDir, 'package.json'));
     fs.writeFileSync(path.resolve(projectDir, 'package.json'), JSON.stringify({ name: 'project' }), 'utf8');
 
-    const argv = ['--projectName=testingApp', '--startCommand="npm start"', '--frontendScripts', `--projectDir=${projectDir}`];
-    await Init.run(argv);
+    await Init.run(['--projectName=testingApp', '--startCommand="npm start"', '--frontendScripts', `--projectDir=${projectDir}`]);
 
     const packageJson = JSON.parse(fs.readFileSync(path.resolve(projectDir, 'package.json'), 'utf8'));
 
