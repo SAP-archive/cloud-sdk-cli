25719bef9e39f3e1126cb27f154d51d6b4db0d7a
Florian Richter <florian.richter@sap.com>
2019-12-09 16:37:43 +0100

    Generate analytics file with {enabled: false} (#25)

--- src/utils/usage-analytics.ts
+++ src/utils/usage-analytics.ts
@@ -7,12 +7,15 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export async function usageAnalytics(projectDir: string, agreeToAnalytics: boolean, salt?: string) {
+  const analyticsFilePath = path.resolve(projectDir, 'sap-cloud-sdk-analytics.json');
   if (agreeToAnalytics === false) {
-    return;
+    return fs.writeFileSync(analyticsFilePath, JSON.stringify({ enabled: false }));
   }
 
   if (agreeToAnalytics || (await cli.confirm('Do you want to provide anonymous usage analytics to help us improve the SDK? (y|n)'))) {
     const jsonContent = salt ? { enabled: true, salt } : { enabled: true };
-    fs.writeFileSync(path.resolve(projectDir, 'sap-cloud-sdk-analytics.json'), JSON.stringify(jsonContent));
+    fs.writeFileSync(analyticsFilePath, JSON.stringify(jsonContent));
+  } else {
+    fs.writeFileSync(analyticsFilePath, JSON.stringify({ enabled: false }));
   }
 }
