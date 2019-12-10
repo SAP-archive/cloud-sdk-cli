8028591e5142fa322b7ee8777c03a6aa9ae8304f
Florian Richter <florian.richter@sap.com>
2019-12-10 14:45:38 +0100

    Add test for jest config util

---
+++ test/utils/jest-config.spec.ts
@@ -0,0 +1,39 @@
+/*!
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+import * as fs from 'fs';
+import * as path from 'path';
+import * as rm from 'rimraf';
+import { getJestConfig, modifyJestConfig } from '../../src/utils';
+
+const pathPrefix = path.resolve(__dirname, __filename.replace(/\./g, '-')).replace('-ts', '');
+
+function getCleanProjectDir(name: string) {
+  const projectDir = path.resolve(pathPrefix, name);
+  if (fs.existsSync(projectDir)) {
+    rm.sync(projectDir);
+  }
+  fs.mkdirSync(projectDir, { recursive: true });
+  return projectDir;
+}
+
+describe('Git Ignore Utils', () => {
+  afterAll(() => {
+    rm.sync(pathPrefix);
+  });
+
+  it('returns jest config object for tests', () => {
+    expect(getJestConfig(true)).toMatchSnapshot();
+    expect(getJestConfig(false)).toMatchSnapshot();
+  });
+
+  it('returns adds jest config to file', () => {
+    const projectDir = getCleanProjectDir('jest-config');
+    const jestConfigPath = path.resolve(projectDir, 'jest-e2e.json');
+    fs.copyFileSync(path.resolve('test', 'nest', 'test', 'jest-e2e.json'), jestConfigPath);
+
+    modifyJestConfig(jestConfigPath, getJestConfig(false));
+
+    expect(JSON.parse(fs.readFileSync(jestConfigPath, { encoding: 'utf8' }))).toMatchObject(getJestConfig(false));
+  });
+});
