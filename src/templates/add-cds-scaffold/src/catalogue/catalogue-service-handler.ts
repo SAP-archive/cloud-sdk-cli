e318054e4caf4a3951b6d2f8d3b2c0585121f5f3
Marika Marszalkowski <marika.marszalkowski@sap.com>
2019-12-20 17:04:18 +0100

    Fix service

--- src/templates/add-cds-scaffold/src/catalogue/catalogue-service-handler.ts
+++ src/templates/add-cds-scaffold/src/catalogue/catalogue-service-handler.ts
@@ -5,13 +5,12 @@ import cds = require('@sap/cds');
 
 // Custom service handler
 export const serviceHandler = srv => {
-
   // After READ Hook for GET requests
   srv.after('READ', 'Books', books => {
     books
-    .filter(book => book.stock > 111)
-    .forEach(book => {
-      book.title += ' -- 11% discount!';
-    });
+      .filter(book => book.stock > 111)
+      .forEach(book => {
+        book.title += ' -- 11% discount!';
+      });
   });
 };
