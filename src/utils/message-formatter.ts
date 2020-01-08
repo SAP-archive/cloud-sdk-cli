cdc2707b944a1f5156568e72876cfa4803ba222e
Florian Richter <florian.richter@sap.com>
2020-01-08 17:45:43 +0100

    Replace boxen with simpler implementation (#44)

--- src/utils/message-formatter.ts
+++ src/utils/message-formatter.ts
@@ -2,20 +2,8 @@
  * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
  */
 
-// The boxen dependency creates this warning in ci: Opening `/dev/tty` failed (6): Device not configured
-// It is likely an issue of github actions: https://github.com/actions/runner/issues/241
-// If this becomes an issue we will have to consider other solutions than boxen
-import boxen = require('boxen');
-
 export function boxMessage(lines: string[]): string {
-  return boxen(lines.join('\n'), {
-    borderStyle: {
-      topLeft: '+',
-      topRight: '+',
-      bottomLeft: '+',
-      bottomRight: '+',
-      horizontal: '-',
-      vertical: ' '
-    }
-  });
+  const lineLength = lines.reduce((prev, curr) => (prev < curr.length ? curr.length : prev), 0);
+  const sep = `+${[...Array(lineLength)].map(() => '-').join('')}+`;
+  return ['', sep, ...lines.map(line => ` ${line} `), sep].join('\n');
 }
