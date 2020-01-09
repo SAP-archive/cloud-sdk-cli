ae1ecf8bd7c2f06758fa75b1e004228139def553
Florian Richter <florian.richter@sap.com>
2020-01-09 15:11:28 +0100

    Fix faulty tests

--- src/commands/help-page.ts
+++ src/commands/help-page.ts
@@ -3,13 +3,16 @@
  */
 
 import { Command } from '@oclif/command';
+import cli from 'cli-ux';
 
 export default class HelpPage extends Command {
   static description = 'Display the product page, which contains tutorials and links to all relevant resources';
 
   async run() {
     this.log('Visit us at:');
-    this.log('  https://developers.sap.com/topics/cloud-sdk.html');
     this.log('  https://community.sap.com/topics/cloud-sdk');
+    this.log('  https://developers.sap.com/topics/cloud-sdk.html');
+
+    await cli.open('https://community.sap.com/topics/cloud-sdk');
   }
 }
