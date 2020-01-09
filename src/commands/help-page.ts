2a673fe2d7629bf59578d524cc88e2abb8485ae7
Florian Richter <florian.richter@sap.com>
2020-01-09 15:34:18 +0100

    Fix async problem in help-page command (#48)
    * Fix async problem in help-page command
    
    * Adjust timeout and remove unnecessary tests
    
    * Fix compilation error
    
    * Fix faulty tests

--- src/commands/help-page.ts
+++ src/commands/help-page.ts
@@ -2,19 +2,17 @@
  * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
  */
 
-import { Command, flags } from '@oclif/command';
+import { Command } from '@oclif/command';
 import cli from 'cli-ux';
 
 export default class HelpPage extends Command {
-  static description = 'Open the product page, which contains tutorials and links to all relevant resources';
-
-  static flags = {
-    help: flags.help({ char: 'h' })
-  };
+  static description = 'Display the product page, which contains tutorials and links to all relevant resources';
 
   async run() {
-    this.log('Visit us at: ');
-    cli.url('https://developers.sap.com/topics/cloud-sdk.html', 'https://developers.sap.com/topics/cloud-sdk.html');
-    cli.open('https://developers.sap.com/topics/cloud-sdk.html');
+    this.log('Visit us at:');
+    this.log('  https://community.sap.com/topics/cloud-sdk');
+    this.log('  https://developers.sap.com/topics/cloud-sdk.html');
+
+    await cli.open('https://community.sap.com/topics/cloud-sdk');
   }
 }
