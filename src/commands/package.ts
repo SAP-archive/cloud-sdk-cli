8184b04cebc7e68cfc2cf4bb16699ecfca89f266
Florian Richter <florian.richter@sap.com>
2020-01-09 12:32:49 +0100

    Alternative solution with --force

--- src/commands/package.ts
+++ src/commands/package.ts
@@ -103,10 +103,9 @@ export default class Package extends Command {
         title: 'Install productive dependencies',
         enabled: () => !flags.skipInstall,
         task: async () =>
-          execa('npm', ['install', '--production'], {
-            cwd: path.resolve('outputDir'),
-            stdio: flags.verbose ? 'inherit' : 'ignore'
-          }).catch(e => this.error(e, { exit: 10 }))
+          execa('npm', ['install', '--production', '--prefix', outputDir, '--force'], { stdio: flags.verbose ? 'inherit' : 'ignore' }).catch(e =>
+            this.error(e, { exit: 10 })
+          )
       }
     ]);
 
