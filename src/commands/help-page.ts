71cd99f758db2873b99a8c8f16921a93788c33b2
Florian Richter <florian.richter@sap.com>
2020-01-09 14:55:15 +0100

    Fix compilation error

--- src/commands/help-page.ts
+++ src/commands/help-page.ts
@@ -2,8 +2,7 @@
  * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
  */
 
-import { Command, flags } from '@oclif/command';
-import cli from 'cli-ux';
+import { Command } from '@oclif/command';
 
 export default class HelpPage extends Command {
   static description = 'Display the product page, which contains tutorials and links to all relevant resources';
