c01d30cb2357996cdad205958717c4fe1dfd6a7c
Florian Richter <florian.richter@sap.com>
2020-01-13 16:40:31 +0100

    Change label to E2E

--- test/utils/scaffold.spec.ts
+++ test/utils/scaffold.spec.ts
@@ -38,7 +38,7 @@ describe('Scaffold Utils', () => {
     expect(await shouldBuildScaffold(projectDir, false)).toBe(false);
   });
 
-  it('[SLOW] should build the scaffold', async () => {
+  it('[E2E] should build the scaffold', async () => {
     const projectDir = getCleanProjectDir(testOutputDir, 'build-scaffold');
 
     await buildScaffold(projectDir, false, false);
