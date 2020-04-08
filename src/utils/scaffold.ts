c949bdba771316c075761d543ba56d3a45d59740
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-04-08 12:55:11 +0200

    Apply suggestions from code review
    Co-Authored-By: Marika Marszalkowski <marika.marszalkowski@sap.com>

--- src/utils/scaffold.ts
+++ src/utils/scaffold.ts
@@ -19,9 +19,9 @@ export async function shouldBuildScaffold(projectDir: string, buildScaffold: boo
     return false;
   }
 
-  cli.log('The target folder (' + projectDir + ') does not contain a `package.json`.');
+  cli.log(`The target directory (${projectDir}) does not contain a \`package.json.\``);
 
-  if (await cli.confirm('Should a new `nest.js` project be initialized in the target folder? (y|n)')) {
+  if (await cli.confirm('Should a new `nest.js` project be initialized in the target directory? (y|n)')) {
     await checkForEmptyDir(projectDir, force);
     return true;
   }
