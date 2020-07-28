1d74f4b16221dc30bf242816f716525033f6958d
Frank Essenberger <frank.essenberger@sap.com>
2020-07-28 15:34:29 +0200

    ten times more timeout

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -54,9 +54,9 @@ export async function deleteAsync(path: string, busyRetries: number): Promise<vo
 }
 
 export enum TimeThresholds {
-  DEFAULT = 5000,
-  SHORT = 30000,
-  MEDIUM = 60000,
-  LONG = 120000,
-  EXTRAO_LONG = 240000
+  DEFAULT = 50000,
+  SHORT = 300000,
+  MEDIUM = 600000,
+  LONG = 1200000,
+  EXTRAO_LONG = 2400000
 }
