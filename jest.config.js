aa5525a7f2240b6eb9cba06aabfb4534b9226564
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-10-28 08:42:17 +0100

    Remove verbose flag

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /* Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. */
 module.exports = {
-  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
